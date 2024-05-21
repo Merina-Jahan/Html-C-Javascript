fd4cd57322699e23263a85921a522d889e4c7666
merinajahan <44259753+merinajahan@users.noreply.github.com>
2024-05-21 13:34:17 +0900

    Add files via upload

---
+++ Area_shapes/index.js
@@ -0,0 +1,29 @@
+
+// Triangle area 
+
+var base = parseFloat(prompt("Enter Base ="));
+var height = parseFloat(prompt("Enter Height = "));
+
+var triangle_area = (base * height)/2;
+
+document.write("triangle_area = " + triangle_area);
+
+
+
+// parseFloat is a function.
+/*
+var base = parseFloat(prompt("Enter Base ="));
+var height = parseFloat(prompt("Enter Height = "));
+
+var square_area = base * height;
+
+document.write("square_area = " + square_area); */
+
+
+
+/* var base = prompt("Enter Base =");
+var height = prompt("Enter Height = ");
+
+var area = base * height;
+
+document.write("area = " + area); */
