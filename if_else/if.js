25d48d284f5c83e1a51ade4e132d4ba7a8f1cf5d
merinajahan <44259753+merinajahan@users.noreply.github.com>
2024-05-23 13:53:04 +0900

    Add files via upload

---
+++ if_else/if.js
@@ -0,0 +1,80 @@
+// Grade calculation
+var num =prompt("Enter your number");
+
+if(num >80 && num <= 100)
+   console.log("A+");
+
+else if(num > 70 && num <=80)
+    console.log("A");
+
+else if(num > 60 && num <= 70 ) 
+
+console.log("A-");
+
+else if(num > 50 && num <=60 )
+console.log("B");
+
+else if(num > 40 && num <=33 )
+console.log("C");
+
+else 
+console.log("Fail");
+
+
+/////////////////
+/*
+var num = prompt("Enter a number: ");
+if(num > 0)
+    console.log("Positive");
+
+else if (num < 0)
+    console.log("Negative");
+
+else
+    console.log("Zero");
+
+
+//positive Negetive print
+var num = prompt("Enter a number: ");
+if(num > 0)
+    console.log("Positive");
+
+if(num < 0)
+    console.log("Negative");
+
+if(num == 0)
+    console.log("Zero");
+*/
+
+
+//////////////////////////
+/*
+
+var num = 5;
+
+if(num%2==0)
+    console.log("Even");
+
+else
+    console.log("odd");
+
+
+/////////////////
+var num = 6;
+
+if(num%2==0)
+    console.log("Even");
+
+if(num%2!=0)
+    console.log("odd");
+
+//////////////////////////
+
+var num = 5;
+
+if(num%2==0)
+    console.log("Even");
+
+if(num%2!=0)
+    console.log("odd");
+*/
