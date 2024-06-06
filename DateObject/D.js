8ae9057995f2d5823426f7434d22a42a99ce0834
merinajahan <44259753+merinajahan@users.noreply.github.com>
2024-06-06 17:28:42 +0900

    Add files via upload

---
+++ DateObject/D.js
@@ -0,0 +1,33 @@
+var date = new Date();
+console.log(date)
+
+var year = date.getFullYear();
+console.log(year)
+
+var month = date.getMonth();
+console.log(month)
+
+var currentDate = date.getDate();
+console.log(currentDate)
+
+var currentDay = date.getDay();
+console.log(currentDay)
+
+var currentHour = date.getHours();
+console.log(currentHour)
+
+var currentMinute = date.getMinutes();
+console.log(currentMinute)
+
+var currentSecond = date.getSeconds();
+console.log(currentSecond)
+
+var currentTime = date.getTime();
+console.log(currentTime)
+
+var Milliseconds = date.getMilliseconds();
+console.log(Milliseconds)
+
+var TimezoneOffset = date.getTimezoneOffset();
+console.log(TimezoneOffset)
+
