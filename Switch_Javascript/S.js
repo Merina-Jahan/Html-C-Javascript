3b988a2fe303844fa882d2f8b7be4b40b4988b1b
merinajahan <44259753+merinajahan@users.noreply.github.com>
2024-06-06 21:04:49 +0900

    Add files via upload

---
+++ Switch_Javascript/S.js
@@ -0,0 +1,96 @@
+//digit spelling
+// 0 - Zero, 1 - one.......9 - Nine, 10 - not a valid digit
+// switch, case
+
+var letter = prompt("Enter any letter: ");
+letter = letter.toLowerCase();
+
+
+switch (letter) {
+    case "a":
+        case "e":
+            case "i":
+                case "o" :
+                    case "u":
+                            console.log("vowel");
+                            break;
+                            default:
+                                console.log("Consonant");
+}
+
+/*
+
+switch (digit){
+    case "0":
+        console.log("Zero");
+        break;
+
+        case "1":
+            console.log("One");
+            break;
+
+            case "2":
+                console.log("Two");
+                break;
+
+                case "3":
+                    console.log("Three");
+                    break;
+
+                    case "4":
+                        console.log("Four");
+                        break;
+
+                        case "5":
+                            console.log("Five");
+                            break;
+
+                            case "6":
+                                console.log("Six");
+                                break;
+
+                                case "7":
+                                    console.log("Seven");
+                                    break;
+
+                                    case "8":
+                                        console.log("Eight");
+                                        break;
+                                        case "9":
+                                            console.log("Nine");
+                                            break;
+
+                                            default:
+                                                console.log("Not a digit");
+}
+*/
+
+
+
+
+
+
+/* var digit = prompt("Enter any digit: ");
+if(digit == 0)
+    console.log("Zero");
+else if(digit == 1)
+    console.log("One");
+else if(digit == 2)
+    console.log("Two");
+else if(digit == 3)
+    console.log("Three");
+else if(digit == 4)
+    console.log("Four");
+else if(digit == 5)
+    console.log("Five");
+else if(digit == 6)
+    console.log("Six");
+else if(digit == 7)
+    console.log("Seven");
+else if(digit == 8)
+    console.log("Eight");
+else if(digit == 9)
+    console.log("Nine");
+
+else
+console.log("Not a digit"); */
