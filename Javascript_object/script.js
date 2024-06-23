e16cb88634867c6b555f857a7db90e4c8b2a5c70
merinajahan <44259753+merinajahan@users.noreply.github.com>
2024-06-23 15:35:55 +0900

    Add files via upload

---
+++ Javascript_object/script.js
@@ -0,0 +1,48 @@
+var myObj = {
+    name: "Rohan",
+    age: 22,
+    email: "Rohan878787@gmail.com",
+    isSingle: true,
+    
+    nextObj: {                 // object declear in object
+        favmovie:"Tufan",
+        favColor: "Black"
+    }
+};
+var x = myObj;
+
+//delete myObj.name;
+
+//myObj.name = "Rifat"; // we can replace the data
+
+// myObj.friends = ["Roki", "Rahat", "Ridom"] // object declear from outside
+console.log(x);
+
+
+
+
+
+
+
+
+
+/* 
+var karim = {
+    color: "black",
+    "Full name": "karimrahman",
+    age: 23,
+    height: 5.5,
+    friends: ["Rahim", "Rahman"]
+};
+
+console.log(karim.friends[1]);
+
+
+console.log(karim["color"]);
+
+console.log(karim.color);
+
+console.log(karim["Full name"]);
+
+*/
+
